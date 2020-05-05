35e9c591e3b7b995e677b4e9608c92e1a392f8b4
5mdlc <gossvald@gmail.com>
2020-05-05 19:36:28 -0300

    add libusb_get_device_list function

--- src/lib-usb.c
+++ src/lib-usb.c
@@ -8,7 +8,7 @@
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
@@ -31,6 +31,39 @@
 #include "xdp-dbus.h"
 #include "xdp-utils.h"
 
+
+
+
+typedef struct _libusb_context_portal
+{
+	int id;
+	libusb_context **context;
+	libusb_device **devs;
+	int cnt_devs;
+} libusb_context_portal;
+
+//libusb_context_portal *context_portal;
+void
+free_context_portal(libusb_context_portal *context_portal)
+{
+	if(context_portal){
+		if(context_portal->context) free(*context_portal->context);
+		context_portal->context = 0L;
+
+		if(context_portal->devs && context_portal->cnt_devs > 0){
+			for(int i = 0; i < context_portal->cnt_devs; i++){
+				free(context_portal->devs[i]);
+				context_portal->devs[i] = 0L;
+			}//endfor
+			free(context_portal->devs) ;
+		}//endif true
+
+		context_portal->devs = 0L;
+
+		context_portal = 0L;
+	}//endif true
+}//end free_context_portal
+
 typedef struct _LibUsb LibUsb;
 typedef struct _LibUsbClass LibUsbClass;
 
@@ -38,14 +71,14 @@ typedef void GLibUsb;
 
 struct _LibUsb
 {
-  XdpLibUsbSkeleton parent_instance;
+	XdpLibUsbSkeleton parent_instance;
 
-  GLibUsb *monitor;
+	GLibUsb *monitor;
 };
 
 struct _LibUsbClass
 {
-  XdpLibUsbSkeletonClass parent_class;
+	XdpLibUsbSkeletonClass parent_class;
 };
 
 static LibUsb *lib_usb;
@@ -54,259 +87,109 @@ GType lib_usb_get_type (void) G_GNUC_CONST;
 static void lib_usb_iface_init (XdpLibUsbIface *iface);
 
 G_DEFINE_TYPE_WITH_CODE (LibUsb, lib_usb, XDP_TYPE_LIB_USB_SKELETON,
-                         G_IMPLEMENT_INTERFACE (XDP_TYPE_LIB_USB, lib_usb_iface_init));
+												 G_IMPLEMENT_INTERFACE (XDP_TYPE_LIB_USB, lib_usb_iface_init));
+
 
 static gboolean
-handle_get_available (XdpLibUsb     *object,
-                      GDBusMethodInvocation *invocation)
+handle_libusb_get_device_list (XdpLibUsb *object,
+									 GDBusMethodInvocation *invocation,
+									gulong context,
+									gulong device_list)
 {
-  Request *request = request_from_invocation (invocation);
-  fprintf(stderr, "lib-usb.c::handle_get_available \n");
+	fprintf(stderr, "lib-usb.c::handle_lib_get_device_list context := 0x%lx, device_list := 0x%lx \n", 
+						 context, device_list);
+	libusb_context_portal *context_portal = (libusb_context_portal *) (context);
 
-  if (!xdp_app_info_has_network (request->app_info))
-    {
-      g_dbus_method_invocation_return_error (invocation,
-                                             XDG_DESKTOP_PORTAL_ERROR,
-                                             XDG_DESKTOP_PORTAL_ERROR_NOT_ALLOWED,
-                                             "This call is not available inside the sandbox");
-    }
-  else
-    {
-      //LibUsb *lu = (LibUsb *)object;
-      gboolean available = TRUE; //g_lib_usb_get_network_available (lu->monitor);
+	context_portal->cnt_devs = libusb_get_device_list(NULL, &context_portal->devs);
+	fprintf(stderr, "devs := 0x%lx, context_portal->cnt_devs := %d\n", (unsigned long)context_portal->devs, context_portal->cnt_devs);
 
-      g_dbus_method_invocation_return_value (invocation, g_variant_new ("(b)", available));
-    }
+	g_dbus_method_invocation_return_value (invocation, g_variant_new ("(i)", context_portal->cnt_devs));
 
-  return TRUE;
+	return TRUE;
 }
 
 static gboolean
-handle_get_metered (XdpLibUsb     *object,
-                    GDBusMethodInvocation *invocation)
+handle_libusb_free_device_list (XdpLibUsb *object,
+									 GDBusMethodInvocation *invocation,
+									gulong context,
+									gulong unref_devices)
 {
-  Request *request = request_from_invocation (invocation);
-  fprintf(stderr, "lib-usb.c::handle_get_metered \n");
+	fprintf(stderr, "lib-usb.c::handle_libusb_free_device_list context := 0x%lx, unref_devices := 0x%lx\n",
+									context, unref_devices);
 
-  if (!xdp_app_info_has_network (request->app_info))
-    {
-      g_dbus_method_invocation_return_error (invocation,
-                                             XDG_DESKTOP_PORTAL_ERROR,
-                                             XDG_DESKTOP_PORTAL_ERROR_NOT_ALLOWED,
-                                             "This call is not available inside the sandbox");
-    }
-  else
-    {
-      //LibUsb *lu = (LibUsb *)object;
-      gboolean metered = TRUE; //g_lib_usb_get_network_metered (lu->monitor);
-
-      g_dbus_method_invocation_return_value (invocation, g_variant_new ("(b)", metered));
-    }
-
-  return TRUE;
+	return TRUE;
 }
 
 static gboolean
-handle_get_connectivity (XdpLibUsb     *object,
-                         GDBusMethodInvocation *invocation)
+handle_libusb_init (XdpLibUsb		 *object,
+									 GDBusMethodInvocation *invocation,
+									gulong									context)
 {
-  Request *request = request_from_invocation (invocation);
-  fprintf(stderr, "lib-usb.c::handle_get_connectivity \n");
+	fprintf(stderr, "lib-usb.c::handle_lib_init context := 0x%lx \n", context);
 
-  if (!xdp_app_info_has_network (request->app_info))
-    {
-      g_dbus_method_invocation_return_error (invocation,
-                                             XDG_DESKTOP_PORTAL_ERROR,
-                                             XDG_DESKTOP_PORTAL_ERROR_NOT_ALLOWED,
-                                             "This call is not available inside the sandbox");
-    }
-  else
-    {
-      //LibUsb *lu = (LibUsb *)object;
-      //GNetworkConnectivity connectivity = g_lib_usb_get_connectivity (lu->monitor);
+	libusb_context_portal *context_portal = malloc(sizeof(libusb_context_portal));
+	memset(context_portal, 0, sizeof(libusb_context_portal));
 
-      //g_dbus_method_invocation_return_value (invocation, g_variant_new ("(u)", connectivity));
-    }
+	gint result = libusb_init((libusb_context **)&context_portal->context);
+	fprintf(stderr, "context_portal := 0x%lx, context := 0x%lx\n", (unsigned long)context_portal,
+	(unsigned long)context_portal->context);
 
-  return TRUE;
+	g_dbus_method_invocation_return_value (invocation, g_variant_new ("(ti)", context_portal, result));
+
+	return TRUE;
 }
 
 static gboolean
-handle_libusb_get_device_list (XdpLibUsb     *object,
-                   GDBusMethodInvocation *invocation,
-                  gint                  context,
-                  gint                  device_list)
+handle_libusb_exit (XdpLibUsb		 *object,
+									 GDBusMethodInvocation *invocation,
+									gulong									context)
 {
-  fprintf(stderr, "lib-usb.c::handle_lib_get_device_list \n");
-  return TRUE;
-}
+	fprintf(stderr, "lib-usb.c::handle_lib_exit context := 0x%lxn", context);
 
-static gboolean
-handle_libusb_free_device_list (XdpLibUsb     *object,
-                   GDBusMethodInvocation *invocation,
-                  gint                  context,
-                  gint                  unref_devices)
-{
-  fprintf(stderr, "lib-usb.c::handle_lib_init \n");
-  return TRUE;
-}
+	libusb_context_portal *context_portal = (libusb_context_portal *) (context);
+	fprintf(stderr, "context_portal := 0x%lx", (unsigned long)context_portal);
+	fprintf(stderr, ", context := 0x%lx\n", (unsigned long)context_portal->context);
 
-static gboolean
-handle_libusb_init (XdpLibUsb     *object,
-                   GDBusMethodInvocation *invocation,
-                  guint                  context)
-{
-  fprintf(stderr, "lib-usb.c::handle_lib_init context := 0x%x \n", context);
+	libusb_exit((libusb_context *)context_portal->context);
 
-  //libusb_device **devs;
-  libusb_context **_context = 0L;
-  fprintf(stderr, "_context := 0x%x\n", (unsigned int)_context);
+	free_context_portal(context_portal);
 
-  //guint _context = 232;
-  gint result = libusb_init((libusb_context **)&_context);
-  fprintf(stderr, "_context := 0x%x\n", (unsigned int)_context);
-
-  g_dbus_method_invocation_return_value (invocation, g_variant_new ("(ui)", _context, result));
-
-  return TRUE;
-}
-
-static gboolean
-handle_libusb_exit (XdpLibUsb     *object,
-                   GDBusMethodInvocation *invocation,
-                  gint                  context)
-{
-  fprintf(stderr, "lib-usb.c::handle_lib_init \n");
-  return TRUE;
-}
-
-static gboolean
-handle_get_status (XdpLibUsb     *object,
-                   GDBusMethodInvocation *invocation)
-{
-  Request *request = request_from_invocation (invocation);
-  fprintf(stderr, "lib-usb.c::handle_get_status \n");
-
-  if (!xdp_app_info_has_network (request->app_info))
-    {
-      g_dbus_method_invocation_return_error (invocation,
-                                             XDG_DESKTOP_PORTAL_ERROR,
-                                             XDG_DESKTOP_PORTAL_ERROR_NOT_ALLOWED,
-                                             "This call is not available inside the sandbox");
-    }
-  else
-    {
-      //LibUsb *lu = (LibUsb *)object;
-      GVariantBuilder status;
-      gboolean b;
-      guint c = 0;
-
-      g_variant_builder_init (&status, G_VARIANT_TYPE_VARDICT);
-      b = TRUE; //g_lib_usb_get_network_available (lu->monitor);
-      g_variant_builder_add (&status, "{sv}",
-                             "available", g_variant_new_boolean (b));
-      //b = g_lib_usb_get_network_metered (lu->monitor);
-      g_variant_builder_add (&status, "{sv}",
-                             "metered", g_variant_new_boolean (b));
-      //c = g_lib_usb_get_connectivity (lu->monitor);
-      g_variant_builder_add (&status, "{sv}", "connectivity", g_variant_new_uint32 (c));
-      g_dbus_method_invocation_return_value (invocation, g_variant_new ("(a{sv})", &status));
-    }
-
-  return TRUE;
-}
-
-	/*
-static void
-can_reach_done (GObject      *source,
-                GAsyncResult *result,
-                gpointer      data)
-{
-  //GLibUsb *monitor = G_LIB_USB (source);
-  //g_autoptr(GDBusMethodInvocation) invocation = data;
-  //gboolean reachable;
-  fprintf(stderr, "lib-usb.c::can_reach_done \n");
-
-  reachable = g_lib_usb_can_reach_finish (monitor, result, NULL);
-
-  g_dbus_method_invocation_return_value (invocation, g_variant_new ("(b)", reachable));
-}
-  */
-
-static gboolean
-handle_can_reach (XdpLibUsb     *object,
-                  GDBusMethodInvocation *invocation,
-                  const char            *hostname,
-                  guint                  port)
-{
-  Request *request = request_from_invocation (invocation);
-  fprintf(stderr, "lib-usb.c::handle_can_reach \n");
-
-  if (!xdp_app_info_has_network (request->app_info))
-    {
-      g_dbus_method_invocation_return_error (invocation,
-                                             XDG_DESKTOP_PORTAL_ERROR,
-                                             XDG_DESKTOP_PORTAL_ERROR_NOT_ALLOWED,
-                                             "This call is not available inside the sandbox");
-    }
-  else
-    {
-      //LibUsb *lu = (LibUsb *)object;
-      g_autoptr(GSocketConnectable) address = NULL;
-
-      address = g_network_address_new (hostname, port);
-      //g_lib_usb_can_reach_async (lu->monitor, address, NULL, can_reach_done, g_object_ref (invocation)); 
-    }
-
-  return TRUE;
-}
-
-static void
-usb_changed (GObject *object,
-                 gboolean network_available,
-                 LibUsb *lu)
-{
-  fprintf(stderr, "lib-usb.c::usb_changed \n");
-  //xdp_lib_usb_emit_changed (XDP_LIB_USB (lu));
+	return TRUE;
 }
 
 static void
 lib_usb_iface_init (XdpLibUsbIface *iface)
 {
-  fprintf(stderr, "lib-usb.c::lib_usb_iface_init \n");
-  iface->handle_get_available = handle_get_available;
-  iface->handle_get_metered = handle_get_metered;
-  iface->handle_get_connectivity = handle_get_connectivity;
-  iface->handle_can_reach = handle_can_reach;
+	fprintf(stderr, "lib-usb.c::lib_usb_iface_init \n");
 
-  iface->handle_libusb_init = handle_libusb_init;
-  iface->handle_libusb_exit = handle_libusb_exit;
-  iface->handle_libusb_get_device_list = handle_libusb_get_device_list;
-  iface->handle_libusb_free_device_list = handle_libusb_free_device_list;
+	iface->handle_libusb_init = handle_libusb_init;
+	iface->handle_libusb_exit = handle_libusb_exit;
+	iface->handle_libusb_get_device_list = handle_libusb_get_device_list;
+	iface->handle_libusb_free_device_list = handle_libusb_free_device_list;
 }
 
 static void
 lib_usb_init (LibUsb *lu)
 {
-  //lu->monitor = g_lib_usb_get_default ();
-  fprintf(stderr, "lib-usb.c::lib_usb_init \n");
+	//lu->monitor = g_lib_usb_get_default ();
+	fprintf(stderr, "lib-usb.c::lib_usb_init \n");
 
-  g_signal_connect (lu->monitor, "usb-changed", G_CALLBACK (usb_changed), lu);
+	//g_signal_connect (lu->monitor, "usb-changed", G_CALLBACK (usb_changed), lu);
 
-  xdp_lib_usb_set_version (XDP_LIB_USB (lu), 1);
+	xdp_lib_usb_set_version (XDP_LIB_USB (lu), 1);
 }
 
 static void
 lib_usb_class_init (LibUsbClass *klass)
 {
-  fprintf(stderr, "lib-usb.c::lib_usb_class_init \n");
+	fprintf(stderr, "lib-usb.c::lib_usb_class_init \n");
 }
 
 GDBusInterfaceSkeleton *
 lib_usb_create (GDBusConnection *connection)
 {
-  fprintf(stderr, "lib-usb.c::lib_usb_create \n");
-  lib_usb = g_object_new (lib_usb_get_type (), NULL);
+	fprintf(stderr, "lib-usb.c::lib_usb_create \n");
+	lib_usb = g_object_new (lib_usb_get_type (), NULL);
 
-  return G_DBUS_INTERFACE_SKELETON (lib_usb);
+	return G_DBUS_INTERFACE_SKELETON (lib_usb);
 }
